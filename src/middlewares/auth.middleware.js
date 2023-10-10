6ad25e1448d276c7e65941c0f6516b99e4aa6e8e
Uriel Campaña <urielcamp@gmail.com>
2023-10-09 21:42:48 -0300

    solucion boton

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,9 @@
 export const privateRoutes = (req, res, next) => {
-    if (req.session.user) return res.redirect('/login');
+    if (!req.session.user) return res.redirect('/login'); // Redirigir a /login si el usuario no está autenticado.
     next();
 }
 
-
 export const publicRoutes = (req, res, next) => {
-    if (!req.session.user) return res.redirect('/')
-    next()
+    if (req.session.user) return res.redirect('/'); // Redirigir a la página de inicio si el usuario ya está autenticado.
+    next();
 }
